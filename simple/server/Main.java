78df2e8343c48ebbd283c67b0834b87f0c908b4b
jupark33 <jupark33@gmail.com>
2018-05-17 09:29:21 +0900

    간단한 소켓 서버

---
+++ simple/server/Main.java
@@ -0,0 +1,26 @@
+package kr.co.ckbs.app.print;
+
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
+
+public class Main {
+
+	private ListenThread listen;
+	private Logger logger;
+
+	public Main() {
+		PropertyConfigurator.configureAndWatch(StringUtil.getWorkingDir() + "/log4j/log4j.properties", 1000);
+		logger = Logger.getLogger(this.getClass());
+	}
+	
+	private void listen() {
+		logger.debug("listening...");
+		listen = new ListenThread(80, 5);
+		Thread th = new Thread(listen);
+		th.start();
+	}
+	
+	public static void main(String[] args) {
+		new Main().listen();
+	}
+}
